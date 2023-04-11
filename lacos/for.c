aa905b5bb6f0085edfc890e4c72a82e2f567ddd3
Esams5 <118021278+Esams5@users.noreply.github.com>
2023-04-10 21:04:08 -0300

    Add files via upload

---
+++ lacos/for.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    // int i;
+    // int x;
+    for (int i = 0; i < 10; i++){
+        // x = i;
+        printf("%d\n",i);
+    }
+
+    return 0;
+
+}
+
+//Estrutura do For em C
