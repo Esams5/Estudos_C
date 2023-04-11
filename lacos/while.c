61124d7f25535a7f643a32b62f6c8100bf76db99
Esams5 <118021278+Esams5@users.noreply.github.com>
2023-04-10 21:05:44 -0300

    Add files via upload

---
+++ lacos/while.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define TRUE 1
+
+
+
+//Estruturas do while!
+
+int main(){
+    int i = 0;
+    
+    // Versão 1.
+    // while(i < 10){
+    //     printf("%d\n",i);
+    //     i++;
+    // }
+
+    // Versão 2
+    // while(i < 10) printf("%d\n",i++);
+
+    // E isso? O que acontece?
+    // while(i < 10);
+    // while(i++ < 10);
+
+    // Loop infinito
+    // while(1){
+    //     printf("OI\n");
+    // }
+
+    while(TRUE){
+        printf("OI\n");
+    }
+    
+
+    return 0;
+
+}
+
